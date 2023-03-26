b6065824a9785d02df7ee532cec9ef769c7f4e68
jake <jake@lucidminds.ai>
2023-03-26 16:39:59 +0100

    initial commit :)

---
+++ README.md
@@ -0,0 +1,1 @@
+sveltekit and tailwind :D
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="w-3/4 mx-auto mt-5 border-4 border-slate-600 min-h-96 bg-white">
+  <h1 class="text-3xl font-bold text-center mx-auto mb-4 p-2">:heart:</h1>
+  <p class="text-lg text-justify m-4 p-4">
+    by Jake
+  </p>
+</div>
+
